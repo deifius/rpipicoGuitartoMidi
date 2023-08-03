92d2426732e10250b771e56265963b5967234dbf
dafe hughes <sunji.roaoul@gmail.com>
2023-08-03 13:44:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -25,16 +25,54 @@ def read_adc() -> int:
     return adc.read_u16()
 
 def frequency_to_midi(frequency: float) -> (int, float):
-    """Maps a frequency to a corresponding MIDI note number and calculates the difference for pitch bend."""
+    """Maps a frequency to a corresponding MIDI note number.
+    
+    Args:
+        frequency (float): The frequency to be mapped.
+
+    Returns:
+        int: Corresponding MIDI note number, float: difference in cents from the midi int
+    """
     midi_note_number = 69 + 12 * np.log2(frequency / 440.0)
     rounded_midi_note = int(round(midi_note_number))
     difference = midi_note_number - rounded_midi_note
     return rounded_midi_note, difference
 
 def calculate_pitch_bend(difference: float) -> int:
-    """Calculates the MIDI pitch bend value."""
+    """Calculates the MIDI pitch bend value for a given frequency and MIDI note.
+    
+    Args:
+        difference (between -1:1, actual pitch distance from midi int)
+
+    Returns:
+        int: Pitch bend value (0-16383).
+    """
     return int(difference * 4096) + 4096  # Assuming a pitch bend range of +/- 1 semitone
 
+def calculate_relative_spl(magnitude: np.array) -> float:
+    """Calculates the relative Sound Pressure Level (SPL) from an FFT magnitude spectrum.
+    
+    Args:
+        magnitude (np.array): Magnitude spectrum from FFT.
+
+    Returns:
+        float: Relative SPL value.
+    """
+    # Implementation depends on specific requirements
+    return magnitude.max()
+
+def calculate_velocity(relative_spl: float) -> int:
+    """Calculates the MIDI velocity based on relative SPL.
+    
+    Args:
+        relative_spl (float): Relative SPL value.
+
+    Returns:
+        int: Corresponding MIDI velocity (0-127).
+    """
+    velocity = int(relative_spl * 127)
+    return min(max(velocity, 0), 127)
+    
 def analyze_pitch_and_send_midi():
     """Analyzes pitch using FFT and sends corresponding MIDI messages."""
     global current_note
